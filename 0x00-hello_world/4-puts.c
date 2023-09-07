6392a26e90a37ba662fe01a4228b72d83aec1b61
kmmadu <mmadubugwukingsley@gmail.com>
2023-09-07 06:31:59 -0700

    4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with put function
- * Return: 0 (Succes) 
+ * Return: 0 (Success) 
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingal puzzle");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }	
