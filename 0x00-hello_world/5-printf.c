7443ae6ac929d9d9921c4d652fae29711a8d006b
kmmadu <mmadubugwukingsley@gmail.com>
2023-09-07 07:01:29 -0700

    5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a message with proper grammar
+ * and is a piece of art, followed by a new line.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
