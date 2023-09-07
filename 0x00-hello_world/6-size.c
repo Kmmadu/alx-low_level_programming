5a4146cf703ae58d042e3f8fdd141a2be17872d3
kmmadu <mmadubugwukingsley@gmail.com>
2023-09-07 07:42:55 -0700

    6-size.c file

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
- *
  * Description: This program prints the size of various data types.
  * Return: 0 (Success)
  */
@@ -13,7 +11,6 @@ int b;
 long int c;
 long long int d;
 float f;
-
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
