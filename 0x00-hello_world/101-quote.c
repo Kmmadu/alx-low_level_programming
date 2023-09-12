71e154b1e80cb0fd33e2ad23a212a2c23051fa1e
kmmadu <mmadubugwukingsley@gmail.com>
2023-09-12 06:56:11 -0700

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
 #include <unistd.h>
 
 /**
  * main - Entry point of the program
- * Description: This program prints a message to the standard error.
- * Return: 1 (Success)
+ * Description: This program prints the specified message to the standard error.
+ * Return: 1 (Error)
  */
 int main(void)
 {
-fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+write(2, message, 59);
 return (1);
 }
+
