cd236c951c020323a1ac9880f1f77d43123af7c7
kmmadu <mmadubugwukingsley@gmail.com>
2023-09-14 04:00:52 -0700

    0-main.c file

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include <main.h>
+
+void positive_or_negative(int i);
+{
+if (i > 0)
+printf("%d is positive\n", i);
+else is (i < 0)
+prinf("%d is negative\n", i);
+else
+prinf("%d is zero \n", i);
+}
