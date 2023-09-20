c0ce1cef47457b09a61271a87ef0e6d703fc1477
kmmadu <mmadubugwukingsley@gmail.com>
2023-09-20 12:22:21 -0700

    104-main.c file

---
+++ 0x06-pointers_arrays_strings/104-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char buffer[] = "This is a string!";
+printf("%s\n", buffer);
+printf("---------------------------------\n");
+print_buffer(buffer, sizeof(buffer));
+return (0);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer in a specific format.
+ * @b: The buffer to be printed.
+ * @size: The number of bytes to print from the buffer.
+ */
+void print_buffer(char *b, int size)
+{
+int i, j;
+if (size <= 0)
+{
+printf("\n");
+return;
+}
+for (i = 0; i < size; i += 10)
+{
+printf("%08x: ", i);
+for (j = i; j < i + 10; j++)
+{
+if (j < size)
+printf("%02x", (unsigned char)b[j]);
+else
+printf("  ");
+if (j % 2 != 0)
+printf(" ");
+}
+for (j = i; j < i + 10; j++)
+{
+if (j < size && b[j] >= 32 && b[j] <= 126)
+printf("%c", b[j]);
+else if (j < size)
+printf(".");
+else
+printf(" ");
+}
+printf("\n");
+}
+}
+
