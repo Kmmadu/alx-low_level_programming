0766f1e047df3172fd13255e2b804ed009d4cf6a
kmmadu <mmadubugwukingsley@gmail.com>
2023-09-20 20:26:43 -0700

    102-magic.c file

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -7,12 +7,18 @@
  */
 int main(void)
 {
+int n;
 int a[5];
-a[2] = 102;
-/* your line of code here */
-*(a + 2) = 98;
-/* your line of code here */
+int *p;
+a[2] = 1024;
+p = &n;
+/*
+ * Here i write my line of code
+ */
+*(p + 5) = 98;
+/*
+ * So they will print 98\n
+ */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
-
