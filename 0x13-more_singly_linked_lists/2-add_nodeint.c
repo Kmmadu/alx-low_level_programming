e96cc9c5ad98cc3c8718406907a12ddc7d53407f
kmmadu <mmadubugwukingsley@gmail.com>
2023-10-25 08:53:12 +0100

    Correction for 2-add_nodeint.c file

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -10,15 +10,15 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-listint_t *new_node;
+listint_t *node;
 if (head == NULL)
 return NULL;
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
+node = malloc(sizeof(listint_t));
+if (node == NULL)
 return NULL;
-new_node->n = n;
-new_node->next = *head;
-*head = new_node;
-return (new_node);
+node->n = n;
+node->next = (*head);
+(*head) = node;
+return (node);
 }
 
