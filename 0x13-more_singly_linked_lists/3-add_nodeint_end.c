fecececd23dc431783610d4890ecd016cfa69374
kmmadu <mmadubugwukingsley@gmail.com>
2023-10-25 09:01:04 +0100

    Just corrected 3-add_nodeint_end.c file

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,23 +10,23 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-listint_t *new_node, *temp;
+listint_t *node, *temp;
 if (head == NULL)
 return (NULL);
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
+node = malloc(sizeof(listint_t));
+if (!node)
 return (NULL);
-new_node->n = n;
-new_node->next = NULL;
+node->n = n;
+node->next = NULL;
 if (*head == NULL)
 {
-*head = new_node;
-return (new_node);
+*head = node;
+return (node);
 }
 temp = *head;
 while (temp->next != NULL)
 temp = temp->next;
-temp->next = new_node;
-return (new_node);
+temp->next = node;
+return (node);
 }
 
